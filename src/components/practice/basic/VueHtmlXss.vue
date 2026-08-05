0d499587f13d08aaaf26ccf6e8d7be5e3236acae
ninano05 <ohjy0530@gmail.com>
2026-08-05 09:55:41 +0900

    feat: 1일차 practice 코드 실습

--- src/components/practice/basic/VueHtmlXss.vue
+++ src/components/practice/basic/VueHtmlXss.vue
@@ -12,5 +12,8 @@ function showMessage() {
 <template>
   <div class="practice-section">
     <h2>v-html XSS 학습</h2>
+    <input v-model="inputValue" placeholder="내용을 입력하세요" />
+    <button @click="showMessage">확인</button>
+    <div v-html="message"></div>
   </div>
 </template>
--- src/views/Day1PracticeView.vue
+++ src/views/Day1PracticeView.vue
@@ -1,14 +1,33 @@
 <!--1일차 실습 바로보기-->
 <script setup>
 // 컴포넌트 가져오기
-
 import EventBasic from '../components/practice/basic/EventBasic.vue'
 import EventModifier from '../components/practice/basic/EventModifier.vue'
 import EventObject from '../components/practice/basic/EventObject.vue'
+
+import ModelBasic from '@/components/practice/basic/ModelBasic.vue'
+import ModelForm from '@/components/practice/basic/ModelForm.vue'
 import ModelModifier from '../components/practice/basic/ModelModifier.vue'
+
 import SampleOne from '../components/practice/basic/SampleOne.vue'
 import SampleTwo from '../components/practice/basic/SampleTwo.vue'
+
+import VueBind from '@/components/practice/basic/VueBind.vue'
+import VueBindClass from '@/components/practice/basic/VueBindClass.vue'
+import VueBindShortedhand from '@/components/practice/basic/VueBindShortedhand.vue'
+import VueBindStyle from '@/components/practice/basic/VueBindStyle.vue'
+import VueCloak from '@/components/practice/basic/VueCloak.vue'
+import VueFor from '@/components/practice/basic/VueFor.vue'
+
 import VueHtml from '../components/practice/basic/VueHtml.vue'
+import VueHtmlXss from '@/components/practice/basic/VueHtmlXss.vue'
+
+import VueIf from '@/components/practice/basic/VueIf.vue'
+import VueMemo from '@/components/practice/basic/VueMemo.vue'
+import VueOnce from '@/components/practice/basic/VueOnce.vue'
+import VuePre from '@/components/practice/basic/VuePre.vue'
+import VueShow from '@/components/practice/basic/VueShow.vue'
+import VueText from '@/components/practice/basic/VueText.vue'
 </script>
 
 <template>
@@ -16,18 +35,32 @@ import VueHtml from '../components/practice/basic/VueHtml.vue'
     <h1>1일차 Vue practice 실습</h1>
 
     <SampleOne></SampleOne>
-
     <SampleTwo></SampleTwo>
 
     <VueHtml></VueHtml>
+    <VueHtmlXss></VueHtmlXss>
 
     <EventBasic></EventBasic>
-
     <EventObject></EventObject>
-
     <EventModifier></EventModifier>
 
+    <ModelBasic></ModelBasic>
+    <ModelForm></ModelForm>
     <ModelModifier></ModelModifier>
+
+    <VueBind></VueBind>
+    <VueBindClass></VueBindClass>
+    <VueBindShortedhand></VueBindShortedhand>
+    <VueBindStyle></VueBindStyle>
+    <VueCloak></VueCloak>
+    <VueFor></VueFor>
+
+    <VueIf></VueIf>
+    <VueMemo></VueMemo>
+    <VueOnce></VueOnce>
+    <VuePre></VuePre>
+    <VueShow></VueShow>
+    <VueText></VueText>
   </div>
 </template>
 
